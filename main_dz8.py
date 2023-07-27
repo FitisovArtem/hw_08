2750bc660d0c3adb6c69d9fc7f74154dc722f93a
FitisovArtem <109963097+FitisovArtem@users.noreply.github.com>
2023-07-27 17:02:00 +0300

    add comments

--- main_dz8.py
+++ main_dz8.py
@@ -3,31 +3,30 @@ from datetime import datetime, timedelta
 
 def get_birthdays_per_week(users):
     result = {}
-    if len(users) < 1:
+    if len(users) < 1:   #Проверка на пустоту списка сотрудников
         return f"Список сотрудников пуст"
     dt_now = datetime.now()
     for employee in users:
-        bd = datetime.strptime(employee.get("birthday"), "%Y-%m-%d")
+        bd = datetime.strptime(employee.get("birthday"), "%Y-%m-%d") #Преобразование полученно даты
         bd_date = datetime(year=bd.year, month=bd.month, day=bd.day)
-        difference = bd_date - datetime(year=dt_now.year, month=dt_now.month, day=dt_now.day)
-        if difference.days >= 0 and difference.days < 7:
-            if bd_date.weekday() == 5:
-                bd_date = bd_date + timedelta(days=2)
-            if bd_date.weekday() == 6:
-                bd_date = bd_date + timedelta(days=1)
-            if bd_date.strftime('%A') not in result:
+        difference = bd_date - datetime(year=dt_now.year, month=dt_now.month, day=dt_now.day) #Получаем разницы между текущей датой и датой рождения сотрудника
+        if difference.days >= 0 and difference.days < 7: # Проверяем что дата рождения сотрудника выпадает на ближ 7 дней
+            if bd_date.weekday() == 5:    #Если ДР в субботу  - 
+                bd_date = bd_date + timedelta(days=2)   # Переносим на понедельник
+            if bd_date.weekday() == 6:    #Если ДР в воскресенье  - 
+                bd_date = bd_date + timedelta(days=1)   # Переносим на понедельник
+            if bd_date.strftime('%A') not in result:    # Собираем в словарь для объединения по дням недели
                 result[bd_date.strftime('%A')] = []
             result[bd_date.strftime('%A')].append(employee.get('name'))
     
-    for key,value in result.items():
-	    print(key, ': ', ", ".join(value))      
-     
-     # print(f"{bd_date.strftime('%A')}: {employee.get('name')}")
-        
+    for key,value in result.items():       # перебираем словарь для вывода имен сотрудников по дням недели 
+	    print(key, ': ', ", ".join(value))              
 
 
 
-users = [{"name": "Ann R","birthday": "2023-07-26"},
+
+
+users = [{"name": "Ann R","birthday": "2022-07-26"},
          {"name": "Li Re","birthday": "2023-07-27"},
          {"name": "Maf RT","birthday": "2023-07-28"},
          {"name": "TRE gh","birthday": "2023-07-29"},
